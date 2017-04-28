e5e46828482a6747ec22f9103e3e62419d7fbefb
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-04-28 13:39:17 -0700

    add integration spec to check word page. spec will pass.

--- spec/kid_dictionary_integration_spec.rb
+++ spec/kid_dictionary_integration_spec.rb
@@ -4,9 +4,18 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the index/word list path', {:type => :feature}) do
-  it('has a button that will allow users to create a new word to add to the word list') do
+  it('has a button with a link that will allow users to create a new word to add to the word list') do
     visit('/')
     click_link('Add a Word')
     expect(page).to have_content('Enter a New Word Here:')
+  end
+end
+
+describe('the add a word path', {:type => :feature}) do
+  it('processes the user entry and adds a new word to the word list') do
+    visit('/word_form')
+    fill_in('word', :with => 'Star')
+    click_button('Add Word')
+    expect(page).to have_content('Star')
   end
 end
