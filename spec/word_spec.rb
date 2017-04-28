79a4820e37b6f78206b0626b16503c900639076c
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-04-28 09:07:54 -0700

    add spec to check id method for Word class. spec will pass.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,9 +3,15 @@ require('word')
 
 describe(Word) do
   describe('#initialize') do
-    it('create a new word') do
+    it('will create a new word') do
       test_word = Word.new('Asteroid')
       expect(test_word.word_name()).to(eq('Asteroid'))
+    end
+  end
+  describe('#initialize') do
+    it('will add an id to the new word') do
+      test_word = Word.new('Asteroid')
+      expect(test_word.id()).to(eq(1))
     end
   end
 end
